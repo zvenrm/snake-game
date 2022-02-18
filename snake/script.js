9c832f34f6bb0b6383a42976f880c4da72ac74db
zvenrm <zvennnnnn@gmail.com>
2022-02-18 13:50:10 +0300

    feat: finish menu and add audio

--- snake/script.js
+++ snake/script.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector('.menu')
+const audio = document.querySelector('.audio')
 const options = document.querySelector('.options')
+const controls = document.querySelector('.controls')
 const gameField = document.querySelector('.game-field')
 const canvas = document.querySelector('.canvas')
 const ctx = canvas.getContext('2d')
@@ -10,14 +12,17 @@ const pauseBtn = document.querySelector('.pause-button')
 const menuBtn = document.querySelector('.menu-btn')
 const menuPlayBtn = document.querySelector('.menu__play-button')
 const menuOptBtn = document.querySelector('.menu__options-button')
+const menuCtrlBtn = document.querySelector('.menu__control-button')
 const menuBackBtn = document.querySelector('.back-button')
+const menuBackBtnCtrl = document.querySelector('.back-button-ctrl')
 const sizeVal = document.querySelector('.size-val')
 const sizeInput = document.querySelector('.size-input')
-
-
+const volVal = document.querySelector('.volume-val')
+const volInput = document.querySelector('.volume-input')
 const resultField = document.querySelector('.result-field')
 const winLose = document.querySelector('.win-lose')
 
+audio.volume = 0.2
 let cellSize = 20 //размер ячейки
 let cellCnt = canvas.width / cellSize //количество ячеек в канвасе
 let dir,
@@ -175,6 +180,124 @@ function onKeyDown(e){
         snakeSpeed.x = 0
         snakeSpeed.y = 1
     }
+    else if(e.keyCode === 80){
+        if(playBtn.textContent === 'Start' || canvas.classList.contains('canvas-none')){
+            e.preventDefault()
+        }
+        else if(pauseBtn.textContent === 'Pause'){
+            pause = true
+            checkKey = 0
+            clearInterval(game)
+            pauseBtn.textContent = 'Play'
+        }
+        else{
+            pause = false
+            game = setInterval(gameUpdate, 100)
+            pauseBtn.textContent = 'Pause'
+        }
+    }
+    else if (e.keyCode === 82){
+        canvas.classList.remove('canvas-none')
+        resultField.classList.remove('result-block')
+        playBtn.textContent = 'Restart'
+        pauseBtn.textContent = 'Pause'
+        pause = false
+        score = 0
+        showScore.textContent = 0
+        clearInterval(game)
+        dir = 'down'
+        snakeSpeed.x = 0
+        snakeSpeed.y = 1
+        snake = []
+        snakeHead = {
+            x: cellCnt / 2,
+            y: cellCnt / 2
+        }
+
+        snakeWidth = 1
+        food = {
+            x: Math.floor(Math.random() * cellCnt),
+            y: Math.floor(Math.random() * cellCnt)
+        }
+        game = setInterval(gameUpdate, 100)
+    }
+    else if (e.keyCode === 27){
+        clearInterval(game)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        menu.classList.remove('menu-none')
+        gameField.classList.remove('game-block')
+        resultField.classList.remove('result-block')
+        canvas.classList.remove('canvas-none')
+        pauseBtn.textContent = 'Pause'
+        playBtn.textContent = 'Start'
+        pause = false
+        score = 0
+        showScore.textContent = 0
+        
+        snakeSpeed = {
+            x: 0,
+            y: 0
+        }
+
+        food = {
+            x: Math.floor(Math.random() * cellCnt),
+            y: Math.floor(Math.random() * cellCnt)
+        }
+
+
+        snake = []
+
+        snakeHead = {
+            x: cellCnt / 2,
+            y: cellCnt / 2
+        }
+
+        snakeWidth = 1
+        game = setInterval(gameUpdate, 100)
+    }
+    else if(e.keyCode === 90){
+        if (audio.volume > 0.1 && audio.muted === true){
+            volInput.value = String(+volInput.value - 1)
+            volVal.textContent = volInput.value * 10
+            audio.volume = +volInput.value / 10
+            audio.muted = false  
+        }
+        else if(audio.volume > 0.1 && audio.muted === false){
+            volInput.value = String(+volInput.value - 1)
+            volVal.textContent = volInput.value * 10
+            audio.volume = +volInput.value / 10
+        }
+        else if (audio.volume === 0.1 && audio.muted === false){
+            audio.muted = true
+        }
+        else{
+            audio.muted = true
+        }
+        console.log(audio.volume)
+    }
+    else if(e.keyCode === 88){
+        if (audio.volume === 0.1 && audio.muted === true){
+            audio.muted = false
+        }
+        else if (audio.volume < 1 && audio.muted === true){
+            volInput.value = String(+volInput.value + 1)
+            volVal.textContent = volInput.value * 10
+            audio.volume = +volInput.value / 10
+            audio.muted = false  
+        }
+        else if(audio.volume < 1 && audio.muted === false){
+            volInput.value = String(+volInput.value + 1)
+            volVal.textContent = volInput.value * 10
+            audio.volume = +volInput.value / 10
+        }
+        else if (audio.volume === 1 && audio.muted === true){
+            audio.muted = false
+        }
+        else{
+            audio.muted = false
+        }
+        console.log(audio.volume)
+    }
 }
 
 function isPauseTrue(){
@@ -283,6 +406,7 @@ menuBtn.addEventListener('click', () => {
 menuPlayBtn.addEventListener('click', () => {
     menu.classList.add('menu-none')
     gameField.classList.add('game-block')
+    audio.play()
 })
 
 menuOptBtn.addEventListener('click', () => {
@@ -290,12 +414,22 @@ menuOptBtn.addEventListener('click', () => {
     options.classList.add('options-block')
 })
 
+menuCtrlBtn.addEventListener('click', () => {
+    menu.classList.add('menu-none')
+    controls.classList.add('controls-block')
+})
+
 menuBackBtn.addEventListener('click', () => {
     menu.classList.remove('menu-none')
     options.classList.remove('options-block')
 })
 
-sizeInput.addEventListener('change', () => {
+menuBackBtnCtrl.addEventListener('click', () => {
+    menu.classList.remove('menu-none')
+    controls.classList.remove('controls-block')
+})
+
+sizeInput.addEventListener('input', () => {
     sizeVal.textContent = sizeInput.value * 10
     cellSize = 400 / sizeVal.textContent
     cellCnt = canvas.width / cellSize
@@ -303,4 +437,9 @@ sizeInput.addEventListener('change', () => {
         x: cellCnt / 2,
         y: cellCnt / 2
     }
+})
+
+volInput.addEventListener('input', () => {
+    volVal.textContent = volInput.value * 10
+    audio.volume = volInput.value / 10
 })
